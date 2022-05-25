63355a1ad77b32c64150518ec7e59649b222c0bb
Akhil mon <akhilmon@hotmail.com>
2022-05-25 09:41:04 +0530

    Add project files.

---
+++ CoreScopedAndTransientDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CoreScopedAndTransientDemo;
+using Microsoft.Extensions.DependencyInjection;
+
+Console.WriteLine("Hello, World!");
+
+
+var collection = new ServiceCollection();
+
+
+//Same
+collection.AddScoped<ScopedClass>();
+
+//Every time new
+collection.AddTransient<TransientClass>();
+
+//ScopedClass scopedClass = new ScopedClass();    
+//TransientClass transientClass   = new TransientClass();
+
+var builder = collection.BuildServiceProvider();
+
+Console.Clear();
+
+Parallel.For(1, 10, i =>
+{
+    Console.WriteLine($"Trancent Instance ID ={ builder.GetService<TransientClass>().GetHashCode().ToString()}");
+
+    Console.WriteLine($"Spoed Instance ID ={ builder.GetService<ScopedClass>().GetHashCode().ToString()}");
+});
+
+
+
+Console.ReadKey();
